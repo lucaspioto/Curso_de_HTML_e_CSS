a5c42edcc1a6821d53f3991850e6d0dcf29568f0
Lucas Tamagnone <lucaspioto@gmail.com>
2021-03-17 15:38:21 -0300

    send e-mail

---
+++ php/envia_mensagem.php
@@ -0,0 +1,28 @@
+<?php
+
+# alterar a variavel abaixo colocando o seu email
+
+$destinatario = "lucaspioto@gmail.com";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$mensagem = $_REQUEST['mensagem'];
+
+ // monta o e-mail na variavel $body
+
+$body = "===================================" . "\n";
+$body = $body . "FALE CONOSCO - TESTE COMPROVATIVO" . "\n";
+$body = $body . "===================================" . "\n\n";
+$body = $body . "Nome: " . $nome . "\n";
+$body = $body . "Email: " . $email . "\n";
+$body = $body . "Mensagem: " . $mensagem . "\n\n";
+$body = $body . "===================================" . "\n";
+
+// envia o email
+mail($destinatario, $body, "From: $email\r\n");
+
+// redireciona para a página de obrigado
+header("location:http://firedlabs.camorii.com.br/#contatos");
+
+
+?>
