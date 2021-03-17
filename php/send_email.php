9114dbcf88a69744b300c0a452f691f47dfee182
Lucas Tamagnone <lucaspioto@gmail.com>
2021-03-17 17:13:40 -0300

    send e-mail

--- php/send_email.php
+++ php/send_email.php
@@ -10,13 +10,11 @@ $mensagem = $_REQUEST['mensagem'];
 
  // monta o e-mail na variavel $body
 
-$body = "===================================" . "\n";
-$body = $body . "FALE CONOSCO - TESTE COMPROVATIVO" . "\n";
-$body = $body . "===================================" . "\n\n";
-$body = $body . "Nome: " . $nome . "\n";
-$body = $body . "Email: " . $email . "\n";
-$body = $body . "Mensagem: " . $mensagem . "\n\n";
-$body = $body . "===================================" . "\n";
+ $body 			= "
+ Nome: 		  ".$nome." 
+ Email: 		".$email."
+ Mensagem:	".$mensagem."
+";
 
 // envia o email
 mail($destinatario, $body, "From: $email\r\n");
