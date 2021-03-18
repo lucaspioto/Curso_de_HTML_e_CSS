a59a1b4e894022847238a9ce2b83f122f09e30ac
Lucas Tamagnone <lucaspioto@gmail.com>
2021-03-18 05:20:18 -0300

    update send e-mail

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -5,12 +5,16 @@ const elements = document.querySelector('.elements');
 
 let pixels = 0;
 
-btnRight.addEventListener('click', function() {
-    pixels = pixels + 50;
-    elements.style = `transform: translateX(${pixels}px)`;
-});
+window.addEventListener('load', () => {
 
-btnLeft.addEventListener('click', function() {
-    pixels = pixels - 50;
-    elements.style = `transform: translateX(${pixels}px)`;
+    btnRight.addEventListener('click', function () {
+        pixels = pixels + 50;
+        elements.style = `transform: translateX(${pixels}px)`;
+    });
+
+    btnLeft.addEventListener('click', function () {
+        pixels = pixels - 50;
+        elements.style = `transform: translateX(${pixels}px)`;
+    });
+
 });
